85d43a41c53c49c5b8e229bb833aec6860b5eef3
Sooyung Kim <kimx1279@gmail.com>
2023-11-11 13:26:19 -0700

    Add validation for bulk discount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,6 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+
+  validates :discount_percent, presence: true, numericality: true
+  validates :threshold, presence: true, numericality: true
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe BulkDiscount, type: :model do
     test_data
   end
 
-  describe "validation" do
+  describe "relationship" do
     it { should belong_to :merchant }
   end
 
+  describe "validations" do
+    it { should validate_presence_of(:discount_percent) }
+    it { should validate_presence_of(:threshold) }
+end
 end
