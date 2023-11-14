495221146f0086e45dacc7aa5cf3bd0a9da05d99
Sooyung Kim <kimx1279@gmail.com>
2023-11-14 10:08:39 -0700

    Add US 8

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -1,84 +1,95 @@
 require "rails_helper"
 
 RSpec.describe "Admin Invoices Show" do
-  before(:each) do
-    test_data
-    @test_invoice = @customer1.invoices.first
-    @test_invoice.update(status: 0, created_at: Time.new(2021, 12, 30))
-    create(:invoice_item, item_id: @item1.id, unit_price: 500, quantity: 5, invoice_id: @test_invoice.id, status: 2)
-    create(:invoice_item, item_id: @item2.id, unit_price: 1850, quantity: 8, invoice_id: @test_invoice.id, status: 2)
-    create(:invoice_item, item_id: @item3.id, unit_price: 1000, quantity: 6, invoice_id: @test_invoice.id, status: 1)
-    create(:invoice_item, item_id: @item4.id, unit_price: 1200, quantity: 10, invoice_id: @test_invoice.id, status: 2)
-  end
-
-  ## USER STORY 33
-  describe 'Admin Invoices Show Page' do
-    before :each do
+  describe 'admin invoice show (pre-existing test)' do
+    before(:each) do
+      test_data
       @test_invoice = @customer1.invoices.first
-      @test_invoice.update(created_at: Time.new(2021, 12, 30))
+      @test_invoice.update(status: 0, created_at: Time.new(2021, 12, 30))
+      create(:invoice_item, item_id: @item1.id, unit_price: 500, quantity: 5, invoice_id: @test_invoice.id, status: 2)
+      create(:invoice_item, item_id: @item2.id, unit_price: 1850, quantity: 8, invoice_id: @test_invoice.id, status: 2)
+      create(:invoice_item, item_id: @item3.id, unit_price: 1000, quantity: 6, invoice_id: @test_invoice.id, status: 1)
+      create(:invoice_item, item_id: @item4.id, unit_price: 1200, quantity: 10, invoice_id: @test_invoice.id, status: 2)
     end
-    it "when visiting show page, returns ID, status, creation date, and full name of customer on invoice" do
+    ## USER STORY 33
+    describe 'Admin Invoices Show Page' do
+      before :each do
+        @test_invoice = @customer1.invoices.first
+        @test_invoice.update(created_at: Time.new(2021, 12, 30))
+      end
+      it "when visiting show page, returns ID, status, creation date, and full name of customer on invoice" do
+        visit "/admin/invoices/#{@test_invoice.id}"
+
+        expect(page).to have_content(@test_invoice.id)
+        expect(page).to have_content(@test_invoice.status.titleize)
+        expect(page).to have_content("Thursday, December 30, 2021")
+        expect(page).to have_content(@test_invoice.customer.name)
+      end
+
+    end
+
+    ## USER STORY 34
+    it "has all of the items on the invoice including its name, quantity, price sold, and status" do
       visit "/admin/invoices/#{@test_invoice.id}"
-
-      expect(page).to have_content(@test_invoice.id)
-      expect(page).to have_content(@test_invoice.status.titleize)
-      expect(page).to have_content("Thursday, December 30, 2021")
-      expect(page).to have_content(@test_invoice.customer.name)
+      @test_invoice.invoice_items.each do |ii|
+        expect(page).to have_content(ii.item.name)
+        expect(page).to have_content(ii.quantity)
+        expect(page).to have_content(ii.price)
+        expect(page).to have_content(ii.status.capitalize)
+      end
     end
 
-  end
+    ## USER STORY 35
+    it "displays the total potential revenue of the invoice" do
+      visit "/admin/invoices/#{@test_invoice.id}"
+      expected_total = 0
+      @test_invoice.invoice_items.each do |ii|
+        expected_total+=(ii.unit_price * ii.quantity)
+      end
+      expected_total = (0.01 * expected_total)
+      expect(page).to have_content("Total Potential Revenue: $#{expected_total}")
+    end
 
-  ## USER STORY 34
-  it "has all of the items on the invoice including its name, quantity, price sold, and status" do
-    visit "/admin/invoices/#{@test_invoice.id}"
-    @test_invoice.invoice_items.each do |ii|
-      expect(page).to have_content(ii.item.name)
-      expect(page).to have_content(ii.quantity)
-      expect(page).to have_content(ii.price)
-      expect(page).to have_content(ii.status.capitalize)
+    ## USER STORY 36
+    it "when visiting the show page, an invoice status can be updated with a select field option, already defaulted as the current status" do
+      visit "/admin/invoices/#{@test_invoice.id}"
+      expect(page).to have_content("In Progress")
+      expect(page).to have_select("status_update", :with_options => ["In Progress", "Completed", "Cancelled"])
+    end
+
+    it "can select a new status and update the invoice - Completed" do
+      visit "/admin/invoices/#{@test_invoice.id}"
+      select "Completed", :from => "status_update"
+      click_button "Update Invoice Status"
+      expect(current_path).to eq("/admin/invoices/#{@test_invoice.id}")
+      expect(page).to have_content("Completed")
+    end
+
+    it "can select a new status and update the invoice - In Progress" do
+      visit "/admin/invoices/#{@test_invoice.id}"
+      select "In Progress", :from => "status_update"
+      click_button "Update Invoice Status"
+      expect(current_path).to eq("/admin/invoices/#{@test_invoice.id}")
+      expect(page).to have_content("In Progress")
+    end
+
+    it "can select a new status and update the invoice - Cancelled" do
+      visit "/admin/invoices/#{@test_invoice.id}"
+      select "Cancelled", :from => "status_update"
+      click_button "Update Invoice Status"
+      expect(current_path).to eq("/admin/invoices/#{@test_invoice.id}")
+      expect(page).to have_content("Cancelled")
     end
   end
 
-  ## USER STORY 35
-  it "displays the total potential revenue of the invoice" do
-    visit "/admin/invoices/#{@test_invoice.id}"
-    expected_total = 0
-    @test_invoice.invoice_items.each do |ii|
-      expected_total+=(ii.unit_price * ii.quantity)
+  describe 'when I visit admin invoice show page' do
+    it 'shows the total revenue and total discounted revenue' do
+      # Solo US 8
+      test_data_4
+      visit "/admin/invoices/#{@invoice30.id}"
+
+      expect(page).to have_content("Total Potential Revenue: $#{@invoice30.potential_revenue}")
+      expect(page).to have_content("Total Discounted Revenue: $#{@invoice30.invoice_items.discounted_revenue}")
     end
-    expected_total = (0.01 * expected_total)
-    expect(page).to have_content("Total Potential Revenue: $#{expected_total}")
   end
-
-  ## USER STORY 36
-  it "when visiting the show page, an invoice status can be updated with a select field option, already defaulted as the current status" do
-    visit "/admin/invoices/#{@test_invoice.id}"
-    expect(page).to have_content("In Progress")
-    expect(page).to have_select("status_update", :with_options => ["In Progress", "Completed", "Cancelled"])
-  end
-
-  it "can select a new status and update the invoice - Completed" do
-    visit "/admin/invoices/#{@test_invoice.id}"
-    select "Completed", :from => "status_update"
-    click_button "Update Invoice Status"
-    expect(current_path).to eq("/admin/invoices/#{@test_invoice.id}")
-    expect(page).to have_content("Completed")
-  end
-
-  it "can select a new status and update the invoice - In Progress" do
-    visit "/admin/invoices/#{@test_invoice.id}"
-    select "In Progress", :from => "status_update"
-    click_button "Update Invoice Status"
-    expect(current_path).to eq("/admin/invoices/#{@test_invoice.id}")
-    expect(page).to have_content("In Progress")
-  end
-
-  it "can select a new status and update the invoice - Cancelled" do
-    visit "/admin/invoices/#{@test_invoice.id}"
-    select "Cancelled", :from => "status_update"
-    click_button "Update Invoice Status"
-    expect(current_path).to eq("/admin/invoices/#{@test_invoice.id}")
-    expect(page).to have_content("Cancelled")
-  end
-
 end
